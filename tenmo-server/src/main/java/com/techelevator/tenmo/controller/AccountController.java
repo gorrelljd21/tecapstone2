51aa40df11603665c17a3053d25b5aaef03fd55d
Randy Brown <rcbwarrior@gmail.com>
2022-07-07 10:48:20 -0400

    getting stuff done

--- tenmo-server/src/main/java/com/techelevator/tenmo/controller/AccountController.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/controller/AccountController.java
@@ -1,7 +1,31 @@
 package com.techelevator.tenmo.controller;
 
+import com.techelevator.tenmo.dao.AccountDao;
+import com.techelevator.tenmo.dao.TransactionDao;
+import com.techelevator.tenmo.exception.AccountNotFoundException;
+import com.techelevator.tenmo.model.Account;
+import com.techelevator.tenmo.model.User;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@PreAuthorize("permitAll")
+@RestController
 public class AccountController {
 
+    private AccountDao dao;
 
-
+    public AccountController(AccountDao accountDao) {
+        this.dao = accountDao;
+    }
+    @GetMapping(path = "/accounts")
+    public List<User> listOfUsers(@RequestParam (defaultValue = "0") int userId) throws AccountNotFoundException {
+        return dao.listOfUsers(userId);
+    }
 }
+
--- tenmo-server/src/main/java/com/techelevator/tenmo/dao/AccountDao.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/AccountDao.java
@@ -1,4 +1,11 @@
 package com.techelevator.tenmo.dao;
 
+import com.techelevator.tenmo.exception.AccountNotFoundException;
+import com.techelevator.tenmo.model.User;
+
+import java.util.List;
+
 public interface AccountDao {
+
+    List<User> listOfUsers (int userId) throws AccountNotFoundException;
 }
